94ed28666eb05bedfd1695c398686f0041785ba8
SchumyHao <schumyhaojl@126.com>
2020-05-25 11:55:09 +0800

    update mklittlefs tools
    Signed-off-by: SchumyHao <schumyhaojl@126.com>

--- LittleFSBuilder.py
+++ LittleFSBuilder.py
@@ -1,2 +1,8 @@
 Import("env")
-env.Replace( MKSPIFFSTOOL='' + env.get("PROJECT_DIR") + '/mklittlefs/mklittlefs' )
+import platform
+import os
+
+print(platform.machine().lower())
+print(platform.system().lower())
+
+env.Replace(MKSPIFFSTOOL='' + env.get("PROJECT_DIR") + '/mklittlefs/' + platform.machine().lower() + '_' + platform.system().lower() + '/mklittlefs' )
